615aab96eeb33e05e60dd8810dca20dfb0200571
shivesh-ranjan <ranjanshivesh71@gmail.com>
2024-09-26 08:06:43 +0530

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  basePath: "/frontend",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
