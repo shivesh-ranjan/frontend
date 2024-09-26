53b8c110423ce7bdd5e478a14357ee6389029015
shivesh-ranjan <ranjanshivesh71@gmail.com>
2024-09-26 10:01:06 +0530

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
   basePath: "/frontend",
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
 
